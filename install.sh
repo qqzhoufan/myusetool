4a0dfe2d23a906c194d5ae9693b489dc474f7dd9
Lazy Fish <237919060@qq.com>
2025-05-02 03:14:23 +0800

    Update install.sh
    增加DD系统的二级页面

--- install.sh
+++ install.sh
@@ -10,43 +10,69 @@ Font_SkyBlue="\033[36m"
 Font_White="\033[37m"
 Font_Suffix="\033[0m"
 
-clear
+# 二级菜单：DD系统选择
+dd_system_menu() {
+    clear
+    echo -e "${Font_Blue}===============================${Font_Suffix}"
+    echo -e "${Font_Blue}     请选择DD系统下载方式       ${Font_Suffix}"
+    echo -e "${Font_Blue}===============================${Font_Suffix}"
+    echo -e "1. 使用curl下载DD"
+    echo -e "2. 使用wget下载DD"
+    echo -e "0. 返回主菜单"
+    echo -e "${Font_Blue}===============================${Font_Suffix}"
+    read -p "请输入数字选择: " sub_choice
 
-echo -e "${Font_Blue}===============================${Font_Suffix}"
-echo -e "${Font_Blue}           咸鱼自用工具箱         ${Font_Suffix}"
-echo -e "${Font_Blue}        	搜集的装机脚本         ${Font_Suffix}"
-echo -e "${Font_Blue}===============================${Font_Suffix}"
-echo -e "${Font_Blue}1. 一键安装docker以及docker compose${Font_Suffix}"
-echo -e "${Font_Blue}2. 安装s-ui脚本${Font_Suffix}"
-echo -e "${Font_Blue}3. NS论坛酒神NodeQuality测试脚本${Font_Suffix}"
-echo -e "${Font_Blue}4. NS论坛酒神DD系统01${Font_Suffix}"
-echo -e "${Font_Blue}5. NS论坛酒神DD系统02${Font_Suffix}"
-echo -e "${Font_Blue}0. 退出${Font_Suffix}"
-echo -e "${Font_Blue}==============================${Font_Suffix}"
-read -p "请输入数字选择操作: " choice
+    case $sub_choice in
+        1)
+            curl -so OsMutation.sh https://raw.githubusercontent.com/LloydAsp/OsMutation/main/OsMutation.sh && chmod u+x OsMutation.sh && ./OsMutation.sh
+            ;;
+        2)
+            wget -qO OsMutation.sh https://raw.githubusercontent.com/LloydAsp/OsMutation/main/OsMutation.sh && chmod u+x OsMutation.sh && ./OsMutation.sh
+            ;;
+        0)
+            return
+            ;;
+        *)
+            echo "无效的选择！"
+            ;;
+    esac
+}
 
-case $choice in
-    1)
-        curl -fsSL https://get.docker.com -o get-docker.sh && chmod +x get-docker.sh && ./get-docker.sh
-        ;;
-    2)
-        curl -Ls https://raw.githubusercontent.com/alireza0/s-ui/master/install.sh -o install.sh && chmod +x install.sh && ./install.sh
-        ;;
-    3)
-        curl -sL https://run.NodeQuality.com
-        ;;
-	4)
-        curl -so OsMutation.sh https://raw.githubusercontent.com/LloydAsp/OsMutation/main/OsMutation.sh && chmod u+x OsMutation.sh && ./OsMutation.sh
-        ;;
-	5)
-        wget -qO OsMutation.sh https://raw.githubusercontent.com/LloydAsp/OsMutation/main/OsMutation.sh && chmod u+x OsMutation.sh && ./OsMutation.sh
-        ;;
-    0)
-        echo "退出程序! "
-        exit 0
-        ;;
-    *)
-        echo "无效的选择！"
-        exit 1
-        ;;
-esac
+# 主菜单循环
+while true; do
+    clear
+    echo -e "${Font_Blue}===============================${Font_Suffix}"
+    echo -e "${Font_Blue}           咸鱼自用工具箱         ${Font_Suffix}"
+    echo -e "${Font_Blue}        搜集的装机脚本         ${Font_Suffix}"
+    echo -e "${Font_Blue}===============================${Font_Suffix}"
+    echo -e "1. 一键安装docker以及docker compose"
+    echo -e "2. 安装s-ui脚本"
+    echo -e "3. NS论坛酒神NodeQuality测试脚本"
+    echo -e "4. NS论坛酒神DD系统（选择下载方式）"
+    echo -e "0. 退出"
+    echo -e "${Font_Blue}===============================${Font_Suffix}"
+    read -p "请输入数字选择操作: " choice
+
+    case $choice in
+        1)
+            curl -fsSL https://get.docker.com -o get-docker.sh && chmod +x get-docker.sh && ./get-docker.sh
+            ;;
+        2)
+            curl -Ls https://raw.githubusercontent.com/alireza0/s-ui/master/install.sh -o install.sh && chmod +x install.sh && ./install.sh
+            ;;
+        3)
+            curl -sL https://run.NodeQuality.com
+            ;;
+        4)
+            dd_system_menu
+            ;;
+        0)
+            echo "退出程序! "
+            exit 0
+            ;;
+        *)
+            echo "无效的选择！"
+            sleep 1
+            ;;
+    esac
+done
